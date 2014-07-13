f19cfee24538fa93a51a61423b5fae383bdb6529
dimas-lex <dimaslex@gmail.com>
2014-07-13 16:06:54 +0300

    ang changes

--- autostop/static/js/app.js
+++ autostop/static/js/app.js
@@ -1,108 +1,77 @@
 'use strict';
-var App = (function() {
-    if (!String.format) {
-        String.format = function(format) {
-            var args = Array.prototype.slice.call(arguments, 1);
-            return format.replace(/{(\d+)}/g, function(match, number) {
-                return typeof args[number] !== 'undefined' ? args[number] : match;
-            });
-        };
-    }
-    String.prototype.format = function() {
-        var str = this.toString();
-        var args = Array.prototype.slice.call(arguments);
-        args.unshift(str);
+AutoApp.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+});
 
-        return String.format.apply(null, args);
-    };
+// configure our routes
+scotchApp.config(function($routeProvider) {
+    $routeProvider
 
-    $(document).ajaxSend(function(event, xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie !== '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
-        }
+    // route for the home page
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+    })
 
-        function sameOrigin(url) {
-            // url could be relative or scheme relative or absolute
-            var host = document.location.host; // host + port
-            var protocol = document.location.protocol;
-            var sr_origin = '//' + host;
-            var origin = protocol + sr_origin;
-            // Allow absolute or scheme relative URLs to same origin
-            return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-                (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-        }
+    // route for the about page
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
+    })
 
-        function safeMethod(method) {
-            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-        }
-
-        if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
+    // route for the contact page
+    .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'contactController'
     });
-    jQuery.ajaxFormSubmit = function(form, onSuccess, onFail, scope) {
-        if (!form) {
-            return onFail.call(scope);
-        }
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize()
-        }).done(
-            onSuccess.bind(scope)
-        ).fail(
-            onFail.bind(scope)
-        );
-    };
+});
 
-    return {
-        Utils: {
-            getDataFromRow: function(cell) {
-                if (!cell) {
-                    return {};
-                }
-                return App.Utils.getDataFrom($(cell).closest('tr').children("td"));
-            },
-            getDataFrom: function(fields) {
-                var values = {};
-                if (fields) {
-                    fields.each(function() {
-                        var key = $.trim($(this).data('name'));
-                        var value = $.trim($(this).text());
-                        values[key] = value;
-                    });
-                }
+AutoApp.config(['$httpProvider',
+    function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-CSRFToken'] = AutoApp._csrf_token;
+        // $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        // $httpProvider.defaults.transformRequest = [
 
-                return values;
-            }
-        }
-    };
+        //     function(data) {
+        //         /**
+        //          * The workhorse; converts an object to x-www-form-urlencoded serialization.
+        //          * @param {Object} obj
+        //          * @return {String}
+        //          */
+        //         var param = function(obj) {
+        //             var query = '';
+        //             var name, value, fullSubName, subName, subValue, innerObj, i;
 
-})();
-var Utils = (function() {
-    return {
-        createUUID: function() {
-            // http://www.ietf.org/rfc/rfc4122.txt
-            //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(chr) {
-                var rnd = Math.random() * 16 | 0,
-                    val = chr === 'x' ? rnd : (rnd & 0x3 | 0x8);
-                return val.toString(16);
-            }).toUpperCase();
-        }
-    };
-})();
+        //             for (name in obj) {
+        //                 value = obj[name];
+
+        //                 if (value instanceof Array) {
+        //                     for (i = 0; i < value.length; ++i) {
+        //                         subValue = value[i];
+        //                         fullSubName = name + '[' + i + ']';
+        //                         innerObj = {};
+        //                         innerObj[fullSubName] = subValue;
+        //                         query += param(innerObj) + '&';
+        //                     }
+        //                 } else if (value instanceof Object) {
+        //                     for (subName in value) {
+        //                         subValue = value[subName];
+        //                         fullSubName = name + '[' + subName + ']';
+        //                         innerObj = {};
+        //                         innerObj[fullSubName] = subValue;
+        //                         query += param(innerObj) + '&';
+        //                     }
+        //                 } else if (value !== undefined && value !== null) {
+        //                     query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        //                 }
+        //             }
+
+        //             return query.length ? query.substr(0, query.length - 1) : query;
+        //         };
+
+        //         return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+        //     }
+        // ]; test
+    }
+]);
